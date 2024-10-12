5e8132d86bf1f60139922fd65c092fd0114cca23
AuroraHan <1224195854@qq.com>
2024-10-12 20:42:31 +0800

    初始化mapbox

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      "/tile": {
+        target: "http://127.0.0.1:8099/tile/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tile/, ""),
+      },
+      "/geoserverApi": {
+        target: "http://127.0.0.1:8099/geoserverApi/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/geoserverApi/, ""),
+      },
+    },
+  },
+});
