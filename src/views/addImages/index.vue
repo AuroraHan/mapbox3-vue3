d2353e35d88f89e786a38b98bf7ce5eaf6fd0c7e
AuroraHan <1224195854@qq.com>
2024-10-14 21:07:40 +0800

    添加弹出框、全局事件

--- src/views/addImages/index.vue
+++ src/views/addImages/index.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, createApp } from 'vue'
 import mapbox from 'mapbox-gl';
+import MPop from '../popup/mPop.vue';
+import emitter from '@/mitt/index'
 
 onMounted(() => {
     initMap()
@@ -22,33 +24,33 @@ const initMap = () => {
     const map = new mapbox.Map({
         container: 'map',
         projection: "globe",
-        // style: 'mapbox://styles/mapbox/outdoors-v12',
-        style: {
-            version: 8,
-            sources: {
-                m_mono: {
-                    type: "raster",
-                    tiles: ["/tile/google/{z}/{x}/{y}.jpg"],
-                    tileSize: 256,
-                    attribution: "",
-                },
-            },
-            glyphs: "../../static/glyphs/{fontstack}/{range}.pbf",
-            layers: [
-                {
-                    id: "m_mono",
-                    type: "raster",
-                    source: "m_mono",
-                    minZoom: 0,
-                    maxZoom: 18,
-                },
-            ],
-            fog: {
-                range: [0.8, 8],
-                color: '#e6ddec',
-                "horizon-blend": 0.1
-            }
-        },
+        style: 'mapbox://styles/mapbox/outdoors-v12',
+        // style: {
+        //     version: 8,
+        //     sources: {
+        //         m_mono: {
+        //             type: "raster",
+        //             tiles: ["/tile/google/{z}/{x}/{y}.jpg"],
+        //             tileSize: 256,
+        //             attribution: "",
+        //         },
+        //     },
+        //     glyphs: "../../static/glyphs/{fontstack}/{range}.pbf",
+        //     layers: [
+        //         {
+        //             id: "m_mono",
+        //             type: "raster",
+        //             source: "m_mono",
+        //             minZoom: 0,
+        //             maxZoom: 18,
+        //         },
+        //     ],
+        //     fog: {
+        //         range: [0.8, 8],
+        //         color: '#e6ddec',
+        //         "horizon-blend": 0.1
+        //     }
+        // },
         center: [120, 30],
         zoom: 2,
     })
@@ -58,6 +60,22 @@ const initMap = () => {
 
     })
 
+    map.on('click', (e) => {
+        customPopup([e.lngLat.lng, e.lngLat.lat])
+    })
+
+}
+
+emitter.on('test', (e) => {
+    console.log(e, 'gggggg');
+
+})
+
+const customPopup = (e) => {
+    let popup = new mapbox.Popup()
+    const container = document.createElement('div')
+    createApp(MPop, { title: 'gggg' }).mount(container)
+    popup.setLngLat(e).setDOMContent(container).addTo(mapR)
 }
 
 const addWmsServer = () => {
---
+++ src/views/popup/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import MPop from './mPop.vue'
+
+</script>
+
+<style scoped></style>
