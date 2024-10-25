b51c413edb1ee2cc5c730418f73a73d092b1f699
AuroraHan <1224195854@qq.com>
2024-10-25 21:03:52 +0800

    添加切换图层页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const routes = [
     { path: "/", component: () => import('@/views/addImages/index.vue') },
     { path: "/addImages", name: 'addImages', component: () => import('@/views/addImages/index.vue') },
     { path: "/popup", name: 'popup', component: () => import('@/views/popup/index.vue') },
+    { path: "/mapboxLayer", name: 'mapboxLayer', component: () => import('@/views/mapboxLayer/index.vue') },
 
 ];
 
---
+++ src/views/mapboxLayer/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="map">
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+import mapbox from 'mapbox-gl';
+
+let mapR: mapboxgl.Map | null = null;
+
+onMounted(() => {
+    initMap()
+})
+
+onUnmounted(() => {
+    mapR?.remove()
+    mapR = null;
+})
+
+const initMap = () => {
+    mapbox.accessToken = "pk.eyJ1IjoiaHBqbmYiLCJhIjoiY20yMzU5OGhzMDI2NjJrb2kweG5yYWRuZSJ9.HX3dEC4HuYwKuA3_Fm2wXA";
+    const map = new mapbox.Map({
+        container: 'map',
+        projection: "mercator",
+        style: 'mapbox://styles/mapbox/outdoors-v12',
+        center: [120, 30],
+        zoom: 2,
+    })
+
+    mapR = map;
+    map.on('load', () => {
+
+    })
+}
+
+
+</script>
+
+<style scoped>
+#map {
+    height: 100vh;
+}
+</style>
