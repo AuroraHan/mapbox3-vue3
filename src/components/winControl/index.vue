b7f0b3596a8f9f5c74be98ec1b37fa8c8f456bc3
AuroraHan <1224195854@qq.com>
2025-03-05 09:53:46 +0800

    全局弹出框调整

--- src/components/winControl/index.vue
+++ src/components/winControl/index.vue
@@ -1,34 +1,65 @@
 <!--  -->
 <template>
     <Vue3DraggableResizable :initW="w" :initH="h" v-model:x="x" v-model:y="y" v-model:w="w" v-model:h="h"
-        v-model:active="active" :draggable="true" :resizable="false" class="def-class"
+        v-model:active="active" :draggable="true" :resizable="false" :parent="true" class="def-class"
         classNameActive="classNameActive">
-        This is a test example
+        <div class="close" @click="onClose">X</div>
+        <slot></slot>
     </Vue3DraggableResizable>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue'
+import { ref, defineProps, PropType, onMounted, toRefs } from 'vue'
 import Vue3DraggableResizable from 'vue3-draggable-resizable'
 import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
 
 const x = ref(100)
 const y = ref(100)
-const h = ref(140)
+const h = ref(300)
 const w = ref(250)
 
+const props = defineProps({
+    initCss: Object as PropType<{
+        width: number,
+        height: number
+    }>
+})
+
 const active = ref(false)
+
+onMounted(() => {
+    const { initCss } = toRefs(props)
+    if (initCss && initCss.value?.width) {
+        w.value = initCss.value.width
+    }
+
+    if (initCss && initCss.value?.height) {
+        h.value = initCss.value.height
+    }
+})
+
+//点击关闭弹出框
+const onClose = () => {
+
+}
 
 </script>
 <style scoped lang='scss'>
 .def-class {
     background-color: aquamarine;
     user-select: none;
+    padding: 4px;
 }
 
 
 .classNameActive {
-    border-color: #526f82;
+    // border-color: #a1a9af;
     border-radius: 5px;
+}
+
+.close {
+    display: flex;
+    justify-content: end;
+    cursor: pointer;
 }
 </style>
--- src/views/cesiumMeasure/components/coordinates/index.vue
+++ src/views/cesiumMeasure/components/coordinates/index.vue
@@ -1,25 +1,12 @@
 <!--  -->
 <template>
     <WinControl>
-
-    </WinControl>
-    <!-- <div class="m-coordinates">
         <coordPlot></coordPlot>
-    </div> -->
+    </WinControl>
 </template>
 
 <script setup lang='ts'>
 import coordPlot from './coordPlot.vue';
 import WinControl from '../../../../components/winControl/index.vue'
 </script>
-<style scoped lang='scss'>
-.m-coordinates {
-    z-index: 9;
-    position: absolute;
-    right: 10%;
-    bottom: 5%;
-    width: 200px;
-    height: 80px;
-    background-color: antiquewhite;
-}
-</style>
+<style scoped lang='scss'></style>
