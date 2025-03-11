95426155d9045b91e2f6b3a16c462521c895439b
AuroraHan <1224195854@qq.com>
2025-03-11 13:23:57 +0800

    ignore

--- src/views/mapBoxL7/index.vue
+++ src/views/mapBoxL7/index.vue
@@ -4,12 +4,12 @@
 
 <script lang="ts" setup>
 import { onMounted } from 'vue'
-import { Scene, Mapbox, PointLayer } from '@antv/l7'
+import { Scene, Mapbox, PointLayer, GeometryLayer } from '@antv/l7'
 import { useMapbox } from '../../hooks/useMapBox';
 
 let mapBox: mapboxgl.Map | null = null;
 let scene: Scene
-const { getMap } = useMapbox({ container: 'map' })
+const { getMap } = useMapbox({ container: 'map', isOffline: true })
 
 onMounted(() => {
     baseConfig()
@@ -26,8 +26,12 @@ const baseConfig = () => {
     //将mapbox提取出来
     mapBox = scene.map as mapboxgl.Map
 
-    add()
-    addImage()
+    scene.on('loaded', () => {
+        // addDem()
+    })
+
+    // add()
+    // addImage()
 }
 
 const add = () => {
@@ -85,6 +89,28 @@ const addImage = () => {
     }).shape('img1').size(40)
 
     scene.addLayer(img)
+}
+
+const addDem = () => {
+    const layer = new GeometryLayer().shape('plane').style({
+        // width: 0.074,
+        // height: 0.061,
+        // center: [120.1025, 30.2594],
+        widthSegments: 100,
+        heightSegments: 100,
+        terrainClipHeight: 1,
+        mapTexture:
+            '/tile/{z}/{x}/{y}.jpg',
+        terrainTexture:
+            '/dem/{z}/{x}/{y}.png',
+        rgb2height: (r, g, b) => {
+            let h = (r * 255.0 * 256.0 * 256.0 + g * 255.0 * 256.0 + b * 255.0) * 0.1;
+            h = h / 200 - 12750;
+            h = Math.max(0, h);
+            return h;
+        },
+    });
+    scene.addLayer(layer);
 }
 
 </script>
