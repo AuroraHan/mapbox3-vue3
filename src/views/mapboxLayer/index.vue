d68ce9335b7b4aef4913c8ce3365cbe137fb9760
AuroraHan <1224195854@qq.com>
2025-03-27 13:47:01 +0800

    添加风场粒子效果

--- src/views/mapboxLayer/index.vue
+++ src/views/mapboxLayer/index.vue
@@ -31,6 +31,11 @@
             <div>特征值属性</div>
             <el-switch v-model="stateFlag" @change="stateHandle" />
         </div>
+
+        <div class="map-item" @click="globalWind">
+            <div>全球风场</div>
+
+        </div>
     </div>
 </template>
 
@@ -621,6 +626,67 @@ const addStates = () => {
         hoveredPolygonId = null;
     });
 
+}
+
+//根据json添加全球风场数据
+const globalWind = async () => {
+    const res = await fetch('/geojson/result.json').then((res) => res.json())
+    loadWinds(res)
+}
+
+const loadWinds = async (data: any) => {
+    const windList = data;
+    //@ts-ignore
+    window.windLayer = new mapboxWind.WindLayer('wind', windList, {
+        windOptions: {
+            // colorScale: (m: any) => {
+            //     // console.log(m);
+            //     if (m < 0) {
+            //         return '#fff'
+            //     } else if (m > 0 && m < 3.3) {
+            //         return '#ffff00'
+            //     } else {
+            //         return '#ff0000'
+            //     }
+            // },
+            colorScale: [
+                'rgb(36,104, 180)',
+                'rgb(60,157, 194)',
+                'rgb(128,205,193 )',
+                'rgb(151,218,168 )',
+                'rgb(198,231,181)',
+                'rgb(238,247,217)',
+                'rgb(255,238,159)',
+                'rgb(252,217,125)',
+                'rgb(255,182,100)',
+                'rgb(252,150,75)',
+                'rgb(250,112,52)',
+                'rgb(245,64,32)',
+                'rgb(237,45,28)',
+                'rgb(220,24,32)',
+                'rgb(180,0,35)',
+            ],
+            velocityScale: 1 / 30,
+            // paths: 5000,
+            frameRate: 16,
+            maxAge: 60,
+            globalAlpha: 0.9,
+            lineWidth: 0.8,
+            // velocityScale: 0.0004,
+            // velocityScale: () => {
+            // 	const zoom = map.getZoom();
+            // 	return velocityScales[zoom] || 0.01
+            // },
+            // paths: 10000,
+            paths: 3782,
+        },
+        fieldOptions: {
+            wrapX: true,
+        },
+    });
+
+    //@ts-ignore
+    window.windLayer.addTo(mapR)
 }
 </script>
 
