500ba99bdef1d88b007ccdf1c5c5e1a7cf0a8576
AuroraHan <1224195854@qq.com>
2025-05-15 14:37:34 +0800

    修改基础配置、添加模型

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import cesium from "vite-plugin-cesium";
 import path from "path";
+import { resolve } from 'path';
+
+const pathResolve = (dir) => {
+  return resolve(__dirname, '.', dir);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +16,11 @@ export default defineConfig({
   plugins: [vue(), cesium()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "/@": pathResolve('./src/'),
     },
   },
   server: {
+    host: '0.0.0.0', // 服务器地址
     hmr: true,
     port: 4001,
     open: false,
@@ -25,18 +31,18 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/tile/, ""),
       },
       // 地图文件代理
-      // "/dem": {
-      //   target: "http://10.30.222.7:7788/dem",
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/dem/, ""),
-      // },
       "/dem": {
-        target: "http://127.0.0.1:8099/dem",
+        target: "http://10.30.222.7:7788/dem",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/dem/, ""),
       },
+      // "/dem": {
+      //   target: "http://127.0.0.1:8099/dem",
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/dem/, ""),
+      // },
       "/geoserverApi": {
-        target: "http://127.0.0.1:10002/",
+        target: "http://127.0.0.1:10001/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/geoserverApi/, ""),
       },
