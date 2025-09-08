2398352d279b126a4e77a31aedffe87c8672fb1d
AuroraHan <1224195854@qq.com>
2025-09-08 23:06:25 +0800

    添加规范配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,19 @@
+import tsParser from '@typescript-eslint/parser'
+
 export default {
+    //忽视自己
+    ignores: ["eslint.config.js"],
+    //文件配置
+    files: ["**/*.ts"],
+    //规范配置
     rules: {
         "no-unused-vars": "error",
         "no-console": "warn",
+    },
+
+    //针对语言进行配置
+    languageOptions: {
+        //指定解释器
+        parser: tsParser
     }
 }
