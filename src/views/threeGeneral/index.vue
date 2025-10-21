69d2d0904ec943d4e60a18d5e2a2e2e56681c51a
AuroraHan <1224195854@qq.com>
2025-10-21 22:34:23 +0800

    添加gltf文件

--- src/views/threeGeneral/index.vue
+++ src/views/threeGeneral/index.vue
@@ -6,6 +6,7 @@
 import { onMounted } from 'vue'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 let camera, scene, renderer, controls;
 onMounted(() => {
@@ -19,15 +20,21 @@ const loadGLTF = () => {
     camera.position.set(0, 1000, 2000);
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff)
 
     const helper = new THREE.GridHelper(3000, 50, 0x303030, 0x303030);
     scene.add(helper);
 
-    const geometry = new THREE.BoxGeometry(100, 100, 100);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // const geometry = new THREE.BoxGeometry(100, 100, 100);
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
 
+    // const ambient = new THREE.AmbientLight(0xffffff, 0.6);
+    // scene.add(ambient)
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    scene.add(directionalLight);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -37,11 +44,21 @@ const loadGLTF = () => {
     controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
 
+    loadModel()
 
     function animate() {
         renderer.render(scene, camera);
     }
     renderer.setAnimationLoop(animate);
+}
+
+const loadModel = async () => {
+    const loader = new GLTFLoader();
+    const gltf = await loader.loadAsync('/models/aim-9_missile/scene.gltf')
+    const model = gltf.scene
+    model.position.set(0, 100, 0)
+    model.scale.set(100, 100, 100)
+    scene.add(model)
 }
 </script>
 
