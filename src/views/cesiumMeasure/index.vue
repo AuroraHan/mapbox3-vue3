1f135953637fb83a9e6eb5ff7d53c6f5181b2b95
AuroraHan <1224195854@qq.com>
2026-06-04 22:32:17 +0800

    ignore

--- src/views/cesiumMeasure/index.vue
+++ src/views/cesiumMeasure/index.vue
@@ -1,168 +1,165 @@
 <template>
-    <!-- 工具集 -->
-    <div id="cesiumContainer"></div>
-    <div class="tools">
-        <div class="item">
-            <div class="title">标记集</div>
-            <el-switch v-model="flagCoordinates" />
-        </div>
-        <div class="item">
-            <div class="title">测绘集</div>
-            <el-switch v-model="flagMeasureTool" />
-        </div>
-        <img @click="boom" src="/images/school.png" alt="" srcset="">
+  <!-- 工具集 -->
+  <div id="cesiumContainer"></div>
+  <div class="tools">
+    <div class="item">
+      <div class="title">标记集</div>
+      <el-switch v-model="flagCoordinates" />
     </div>
-    <Coordinates v-if="flagCoordinates"></Coordinates>
-    <MeasureTool v-if="flagMeasureTool"></MeasureTool>
+    <div class="item">
+      <div class="title">测绘集</div>
+      <el-switch v-model="flagMeasureTool" />
+    </div>
+    <img @click="boom" src="/images/school.png" alt="" srcset="" />
+  </div>
+  <Coordinates v-if="flagCoordinates"></Coordinates>
+  <MeasureTool v-if="flagMeasureTool"></MeasureTool>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, provide } from 'vue';
-import * as Cesium from 'cesium';
-import { useCesium } from '../../hooks/useCesium'
-import Coordinates from './components/coordinates/index.vue'
-import MeasureTool from './components/measureTool/index.vue'
-import { rotateGlobe } from '../../utils/cesiumTools'
-import { functions } from 'lodash';
+import { onMounted, ref, provide } from "vue";
+import * as Cesium from "cesium";
+import { useCesium } from "../../hooks/useCesium";
+import Coordinates from "./components/coordinates/index.vue";
+import MeasureTool from "./components/measureTool/index.vue";
+import { rotateGlobe } from "../../utils/cesiumTools";
+import { functions } from "lodash";
 
 // import { useCesiumS } from '@/stores/cesiumStore'
 
 let cesiumV: Cesium.Viewer;
-const { getCesiumViewer } = useCesium(
-    {
-        container: 'cesiumContainer',
-        timeline: false,
-        animation: false,
-        infoBox: false,
-        addTerrain: false
-    }
-)
+const { getCesiumViewer } = useCesium({
+  container: "cesiumContainer",
+  timeline: false,
+  animation: false,
+  infoBox: false,
+  addTerrain: false,
+});
 
-const flagCoordinates = ref(false)
-const flagMeasureTool = ref(false)
+const flagCoordinates = ref(false);
+const flagMeasureTool = ref(false);
 
 onMounted(() => {
-    cesiumV = getCesiumViewer()
-    provide('myViewer', cesiumV)
-    // useCesiumS().setCesiumS(unref(cesiumV))
+  cesiumV = getCesiumViewer();
+  provide("myViewer", cesiumV);
+  // useCesiumS().setCesiumS(unref(cesiumV))
 
-    //旋转地球效果
-    // rotateGlobe(cesiumV)
-})
+  //旋转地球效果
+  // rotateGlobe(cesiumV)
+});
 
 //
 
 const onClickImg = () => {
-    document.body.style.cursor = `url(/images/arm/soldier.png) 32 32, help`
-}
+  document.body.style.cursor = `url(/images/arm/soldier.png) 32 32, help`;
+};
 
 //高级爆炸效果
 const boom = () => {
-    let billboardCollection = cesiumV.scene.primitives.add(new Cesium.BillboardCollection());
-    let particles: any = [];
-    // 粒子数量
-    const particleCount = 520;
-    // 爆炸中心（自己传入）
-    const explosionCenter = Cesium.Cartesian3.fromDegrees(120, 31, 1000);
+  let billboardCollection = cesiumV.scene.primitives.add(
+    new Cesium.BillboardCollection(),
+  );
+  let particles: any = [];
+  // 粒子数量
+  const particleCount = 520;
+  // 爆炸中心（自己传入）
+  const explosionCenter = Cesium.Cartesian3.fromDegrees(120, 31, 1000);
 
-    createExplosion()
+  createExplosion();
 
-    function createExplosion() {
-        for (let i = 0; i < particleCount; i++) {
+  function createExplosion() {
+    for (let i = 0; i < particleCount; i++) {
+      // 随机方向（单位向量）
+      const dir = Cesium.Cartesian3.normalize(
+        new Cesium.Cartesian3(
+          Math.random() - 0.5,
+          Math.random() - 0.5,
+          Math.random() - 0.2,
+        ),
+        new Cesium.Cartesian3(),
+      );
 
-            // 随机方向（单位向量）
-            const dir = Cesium.Cartesian3.normalize(
-                new Cesium.Cartesian3(
-                    Math.random() - 0.5,
-                    Math.random() - 0.5,
-                    Math.random() - 0.2
-                ),
-                new Cesium.Cartesian3()
-            );
+      const billboard = billboardCollection.add({
+        image: "/images/fire.png", // 一张小的亮点/烟雾贴图
+        position: explosionCenter,
+        width: 20,
+        height: 20,
+        color: Cesium.Color.WHITE.withAlpha(1.0),
+      });
 
-            const billboard = billboardCollection.add({
-                image: "/images/fire.png",  // 一张小的亮点/烟雾贴图
-                position: explosionCenter,
-                width: 20,
-                height: 20,
-                color: Cesium.Color.WHITE.withAlpha(1.0)
-            });
-
-            particles.push({
-                billboard,
-                dir,                // 扩散方向
-                speed: 30 + Math.random() * 30,  // 初速度
-                life: 1.0           // 生命周期(透明度)
-            });
-        }
-
-        // 使用请求动画帧来优化性能
-        cesiumV.scene.postRender.addEventListener(updateExplosion);
+      particles.push({
+        billboard,
+        dir, // 扩散方向
+        speed: 30 + Math.random() * 30, // 初速度
+        life: 1.0, // 生命周期(透明度)
+      });
     }
 
-    function updateExplosion() {
+    // 使用请求动画帧来优化性能
+    cesiumV.scene.postRender.addEventListener(updateExplosion);
+  }
 
-        particles.forEach(p => {
-            const b = p.billboard;
+  function updateExplosion() {
+    particles.forEach((p) => {
+      const b = p.billboard;
 
-            // 计算新位置（沿 dir 扩散）
-            const pos = new Cesium.Cartesian3();
-            Cesium.Cartesian3.multiplyByScalar(p.dir, p.speed, pos);
-            Cesium.Cartesian3.add(b.position, pos, pos);
+      // 计算新位置（沿 dir 扩散）
+      const pos = new Cesium.Cartesian3();
+      Cesium.Cartesian3.multiplyByScalar(p.dir, p.speed, pos);
+      Cesium.Cartesian3.add(b.position, pos, pos);
 
-            b.position = pos;
+      b.position = pos;
 
-            // 粒子逐渐消失
-            p.life -= 0.015;
-            b.color = Cesium.Color.WHITE.withAlpha(p.life);
+      // 粒子逐渐消失
+      p.life -= 0.015;
+      b.color = Cesium.Color.WHITE.withAlpha(p.life);
 
-            // 粒子逐渐变大（烟雾效果）
-            b.width += 0.5;
-            b.height += 0.5;
+      // 粒子逐渐变大（烟雾效果）
+      b.width += 0.5;
+      b.height += 0.5;
 
-            // 生命周期结束移除
-            if (p.life <= 0) {
-                billboardCollection.remove(b);
-            }
-        });
+      // 生命周期结束移除
+      if (p.life <= 0) {
+        billboardCollection.remove(b);
+      }
+    });
 
-        // 清理结束的粒子
-        particles = particles.filter(p => p.life > 0);
-    }
-}
-
+    // 清理结束的粒子
+    particles = particles.filter((p) => p.life > 0);
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 #cesiumContainer {
-    height: 100vh;
-    width: 100%;
+  height: 100vh;
+  width: 100%;
 }
 
 .tools {
-    position: absolute;
-    top: 2%;
-    left: 3%;
-    width: 200px;
-    // height: 40px;
-    font-size: 18px;
-    border-radius: 3px;
-    background-color: beige;
+  position: absolute;
+  top: 2%;
+  left: 3%;
+  width: 200px;
+  // height: 40px;
+  font-size: 18px;
+  border-radius: 3px;
+  background-color: beige;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .item {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
     align-items: center;
+    line-height: 40px;
+    font-weight: bold;
+    color: rgb(49, 58, 55);
 
-    .item {
-        display: flex;
-        align-items: center;
-        line-height: 40px;
-        font-weight: bold;
-        color: rgb(49, 58, 55);
-
-        .title {
-            margin-right: 10px;
-        }
+    .title {
+      margin-right: 10px;
     }
+  }
 }
 </style>
--- src/views/cesiumPopup/index.vue
+++ src/views/cesiumPopup/index.vue
@@ -1,101 +1,119 @@
 <template>
-    <div id="cesiumContainer"></div>
-    <MyDialog ref="mydialog" :positionXY="pos"></MyDialog>
+  <div id="cesiumContainer"></div>
+  <MyDialog ref="mydialog" :positionXY="pos"></MyDialog>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, reactive } from 'vue';
-import * as Cesium from 'cesium';
-import { useCesium } from '../../hooks/useCesium'
-import MyDialog from '@/components/cesiumPopupTwo/index.vue';
-import DivBillboard from '@/utils/DivBillboard'
-import lineDiv from './lineDiv.vue';
+import { onMounted, ref, reactive } from "vue";
+import * as Cesium from "cesium";
+import { useCesium } from "../../hooks/useCesium";
+import MyDialog from "@/components/cesiumPopupTwo/index.vue";
+import DivBillboard from "@/utils/DivBillboard";
+import lineDiv from "./lineDiv.vue";
 
 let cesiumV: Cesium.Viewer;
-const { getCesiumViewer } = useCesium({ container: 'cesiumContainer', infoBox: false, shouldAnimate: true })
+const { getCesiumViewer } = useCesium({
+  container: "cesiumContainer",
+  infoBox: false,
+  shouldAnimate: true,
+});
 
-const mydialog = ref()
+const mydialog = ref();
 
 onMounted(() => {
-    cesiumV = getCesiumViewer()
-    //弹窗
+  cesiumV = getCesiumViewer();
+  //弹窗
 
-    demoVueComp()
-    demoB()
-})
-
+  demoVueComp();
+  demoB();
+});
 
 //案例A 使用vue组件弹出框
 const pos = reactive({
-    xAxis: 0,
-    yAxis: 0
-})
+  xAxis: 0,
+  yAxis: 0,
+});
 const demoVueComp = () => {
-    //添加实体
-    const entityOne = cesiumV.entities.add({
-        name: '标识',
-        position: Cesium.Cartesian3.fromDegrees(120, 30, 2000),
-        model: {
-            uri: '/models/CesiumDrone.glb',
-            minimumPixelSize: 92,
-            maximumScale: 20000,
-        },
-        description: '描述',
-        data: {
-            name: '公益性公园'
+  //添加实体
+  const entityOne = cesiumV.entities.add({
+    name: "标识",
+    position: Cesium.Cartesian3.fromDegrees(120, 30, 2000),
+    model: {
+      uri: "/models/CesiumDrone.glb",
+      minimumPixelSize: 92,
+      maximumScale: 20000,
+    },
+    description: "描述",
+    data: {
+      name: "公益性公园",
+    },
+  });
+
+  let handle = new Cesium.ScreenSpaceEventHandler(cesiumV.scene.canvas);
+  handle.setInputAction((clickEvent: any) => {
+    let pickData = cesiumV.scene.pick(clickEvent.position);
+
+    //存在则打开弹出框
+    if (Cesium.defined(pickData)) {
+      let scratch = new Cesium.Cartesian2();
+      cesiumV.scene.preRender.addEventListener(() => {
+        //获取模型的坐标数据
+        const modelPos = pickData.primitive.id.position.getValue();
+        let position = new Cesium.Cartesian3(
+          modelPos.x,
+          modelPos.y,
+          modelPos.z,
+        );
+        let canvasPosition = cesiumV.scene.cartesianToCanvasCoordinates(
+          position,
+          scratch,
+        );
+
+        if (Cesium.defined(canvasPosition)) {
+          pos.xAxis = canvasPosition.x - 100;
+          pos.yAxis = canvasPosition.y - 70;
         }
-    })
+      });
 
-    let handle = new Cesium.ScreenSpaceEventHandler(cesiumV.scene.canvas)
-    handle.setInputAction((clickEvent: any) => {
-        let pickData = cesiumV.scene.pick(clickEvent.position)
+      mydialog.value.openDialog();
+    } else {
+      mydialog.value.closeDialog();
+    }
+  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
-        //存在则打开弹出框
-        if (Cesium.defined(pickData)) {
-            let scratch = new Cesium.Cartesian2();
-            cesiumV.scene.preRender.addEventListener(() => {
-                //获取模型的坐标数据
-                const modelPos = pickData.primitive.id.position.getValue()
-                let position = new Cesium.Cartesian3(modelPos.x, modelPos.y, modelPos.z);
-                let canvasPosition = cesiumV.scene.cartesianToCanvasCoordinates(position, scratch);
-
-                if (Cesium.defined(canvasPosition)) {
-                    pos.xAxis = canvasPosition.x - 100
-                    pos.yAxis = canvasPosition.y - 70
-                }
-            })
-
-            mydialog.value.openDialog()
-        } else {
-            mydialog.value.closeDialog()
-        }
-    }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
-
-
-    // setInterval(() => {
-    //     entityOne.position = Cesium.Cartesian3.fromDegrees(100 + Math.random() * 22, 20 + Math.random() * 16, Math.random() * 3000)
-    // }, 4000)
-}
-
+  // setInterval(() => {
+  //     entityOne.position = Cesium.Cartesian3.fromDegrees(100 + Math.random() * 22, 20 + Math.random() * 16, Math.random() * 3000)
+  // }, 4000)
+};
 
 //方案B
 const demoB = () => {
-    const div = {
-        id: "002",
-        name: '这是Vue组件',
-        position: [113, 37, 100],
-        content: '这是一个Vue组件',
-        vueComponent: lineDiv
-    }
+  const div = {
+    id: "002",
+    name: "这是Vue组件",
+    position: [113, 37, 100],
+    content: "这是一个Vue组件",
+    vueComponent: lineDiv,
+  };
 
-    let divbillboard = new DivBillboard(cesiumV, Cesium.Cartesian3.fromDegrees(div.position[0], div.position[1], div.position[2]), div.content, div.vueComponent, true, div.id)
-    // divbillboard.flyTo()
-}
-
+  let divbillboard = new DivBillboard(
+    cesiumV,
+    Cesium.Cartesian3.fromDegrees(
+      div.position[0],
+      div.position[1],
+      div.position[2],
+    ),
+    div.content,
+    div.vueComponent,
+    true,
+    div.id,
+  );
+  // divbillboard.flyTo()
+};
 </script>
 
 <style>
 #cesiumContainer {
-    height: 100vh;
+  height: 100vh;
 }
 </style>
